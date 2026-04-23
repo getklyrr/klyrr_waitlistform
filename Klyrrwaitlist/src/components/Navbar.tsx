81cc8a95c47f74c71e7cae70f76c186864622aac
getklyrr <get.klyrr@gmail.com>
2026-04-24 05:25:21 +0530

    Update Navbar.tsx

--- Klyrrwaitlist/src/components/Navbar.tsx
+++ Klyrrwaitlist/src/components/Navbar.tsx
@@ -1,17 +1,18 @@
 import Image from "next/image";
-export default function Navbar(){
-    return(
-        <nav className="w-full flex-items-center justify-between p-6 bg-cream border-b border-gray-200">
-            <div className="flex items-center">
-                <Image
-                src="/logo.png"
-                alt="Klyrr Logo"
-                width={120}
-                height={40}
-                className="object-contain h-8 w-auto"
-                priority
-                />
-            </div>
-        </nav>
-    );
+
+export default function Navbar() {
+  return (
+    <nav className="w-full flex items-center justify-between p-6 bg-cream border-b border-burgundy/10">
+      <div className="flex items-center">
+        <Image
+          src="/logo.png"
+          alt="Klyrr Logo"
+          width={120}
+          height={40}
+          className="object-contain h-8 w-auto"
+          priority
+        />
+      </div>
+    </nav>
+  );
 }
