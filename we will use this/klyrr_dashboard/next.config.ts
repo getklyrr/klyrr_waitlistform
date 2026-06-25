62ea9055c7a9cde81f8a3b55b35a17d3b37e1d87
Klyrr <get.klyrr@gmail.com>
2026-06-25 17:36:22 +0530

    Update next.config.ts

--- we will use this/klyrr_dashboard/next.config.ts
+++ we will use this/klyrr_dashboard/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     serverActions: {
+      // Correct configuration block nesting for cross-origin or local network routing permissions
       allowedOrigins: ["localhost:3000", "192.168.31.104:3000"],
     },
   },
